ff993f77f2ad9b8e4c70d34adc8104b88e89cd86
Icaro-Salgado <icaro.salgado@mercadolivre.com>
2022-03-28 17:36:02 -0300

    fix: fixing name regex validation

--- src/main/java/br/com/mercadolivre/obterdiploma/model/StudentDTO.java
+++ src/main/java/br/com/mercadolivre/obterdiploma/model/StudentDTO.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class StudentDTO {
 
     @NotBlank(message="O nome do aluno não pode ficar vazio.")
-    @Pattern(regexp = "/^[A-Z][a-z0-9_-]{3,19}$/", message = "O nome do aluno deve começar com letra maiúscula.")
+    @Pattern(regexp = "\\b[A-Z][A-Za-z]*\\b", message = "O nome do aluno deve começar com letra maiúscula.")
     @Size(max=50, message = "O comprimento do nome não pode exceder 50 caracteres.")
     String studentName;
 
